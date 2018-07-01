d0fd6ac4737a5cf46816a56c208b851a172f1b6b
Fufuhu <e238058r@hotmail.co.jp>
2018-07-02 00:30:28 +0900

    ヘッダおよびメッセージボディの値を出力する

--- code/server/main.go
+++ code/server/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World")
+
+	fmt.Printf("%v\n", r)
+
+	for field, values := range r.Header {
+		fmt.Printf("%s\n", field)
+		for _, value := range values {
+			fmt.Printf("	Value=%s\n", value)
+		}
+	}
+
+	body_closer := r.Body
+	bytes_buffer := new(bytes.Buffer)
+
+	bytes_buffer.ReadFrom(body_closer)
+	fmt.Printf("%v\n", bytes_buffer.String())
 }
 
 func main() {
